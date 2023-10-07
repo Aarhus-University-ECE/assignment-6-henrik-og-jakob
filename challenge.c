45c3341551199a775034d6eeeefaeb13cd3977c5
jakoblund11 <lundjakob@live.dk>
2023-10-07 15:26:03 +0200

    Finished challenge

--- challenge.c
+++ challenge.c
@@ -27,7 +27,6 @@ void code_x (int x, int y, int z, int inp[1000][3], int *RAM, int registers[10])
         registers[y] = registers[z];
         break;
     case 6:
-        printf("doing something\n");
         registers[y] = (registers[y] + registers[z]) % 1000;
         break;
     case 7:
@@ -43,14 +42,13 @@ void code_x (int x, int y, int z, int inp[1000][3], int *RAM, int registers[10])
         inp[z][2] = (temp1 - inp[z][0] * 100 - inp[z][1] * 10);
         break;
     case 0:
-        if (registers[z] != 0)
-            *RAM = registers[y];
-        break;
+    if (registers[z] != 0)
+        *RAM = registers[y] - 1;
+    break;
     default:
         break;
     }
 }
-
 
 int main() {
     int inp[1000][3];
@@ -63,37 +61,16 @@ int main() {
 
     int i = 0;
     int temp;
-    // while (1) {
-    //     scanf("%d", &temp);
-    //     inp[i][0] = temp / 100;
-    //     if (temp / 100 == 1) {
-    //         break;
-    //     }
-    //     inp[i][1] = (temp - inp[i][0] * 100) / 10;
-    //     inp[i][2] = (temp - inp[i][0] * 100 - inp[i][1] * 10);
-    //     i++;
-    // }
-
-    int case_inp[12][3] = { {2, 9, 9},
-                            {4, 9, 2},
-                            {4, 9, 5},
-                            {3, 9, 9},
-                            {4, 9, 2},
-                            {4, 9, 5},
-                            {3, 9, 9},
-                            {2, 8, 3},
-                            {2, 7, 9},
-                            {6, 8, 9},
-                            {0, 7, 8},
-                            {1, 0, 0} };
-    
-    for (int i = 0; i < 12; i++) {
-        inp[i][0] = case_inp[i][0];
-        inp[i][1] = case_inp[i][1];
-        inp[i][2] = case_inp[i][2];
+    while (1) {
+        scanf("%d", &temp);
+        inp[i][0] = temp / 100;
+        if (temp / 100 == 1) {
+            break;
+        }
+        inp[i][1] = (temp - inp[i][0] * 100) / 10;
+        inp[i][2] = (temp - inp[i][0] * 100 - inp[i][1] * 10);
+        i++;
     }
-
-    printf("Input registered\n");
     
     int RAM = 0;
     int counter = 0;
@@ -101,27 +78,13 @@ int main() {
     while (RAM < 1000) {
         counter++;
         if (inp[RAM][0] == 1) {
-            printf("stop");
             break;
         }
         if (inp[RAM][0] == 0 && inp[RAM][1] == 0 && inp[RAM][2] == 0) {
             break;
         }
         code_x(inp[RAM][0], inp[RAM][1], inp[RAM][2], inp, &RAM, registers);
-        printf("Current RAM = %d\n", RAM);
         RAM++;
-        printf("Current RAM = %d\n", RAM);
-        // if (RAM == 9) {
-        //     printf("%d %d %d\n", inp[RAM][0], inp[RAM][1], inp[RAM][2]);
-        //     NineCount++;
-        //     if (NineCount > 3) {
-        //         break;
-        //     }
-        // }
-        // else {
-        //     NineCount = 0;
-        // }
-        // RAM++;
     }
     
     printf("%d\n", counter);
--- playground2.c
+++ playground2.c
@@ -1,16 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int arr2num (int arr[3]) {
-    int num = 0;
-    for (int i = 0; i < 3; i++) {
-        num = num * 10 + arr[i];
+void interpreter(int ram[1000]) {
+    int registers[10] = {0};
+    int pc = 0;
+    int executed = 0;
+    while (1) {
+        int instruction = ram[pc];
+        pc += 1;
+        executed += 1;
+        int code = instruction / 100;
+        int d = (instruction / 10) % 10;
+        int n = instruction % 10;
+        if (code == 0) {
+            if (registers[n] != 0) {
+                pc = registers[d];
+            }
+        } else if (code == 1) {
+            printf("%d\n", executed);
+            return;
+        } else if (code == 2) {
+            registers[d] = n;
+        } else if (code == 3) {
+            registers[d] = (registers[d] + n) % 1000;
+        } else if (code == 4) {
+            registers[d] = (registers[d] * n) % 1000;
+        } else if (code == 5) {
+            registers[d] = registers[n];
+        } else if (code == 6) {
+            registers[d] = (registers[d] + registers[n]) % 1000;
+        } else if (code == 7) {
+            registers[d] = (registers[d] * registers[n]) % 1000;
+        } else if (code == 8) {
+            registers[d] = ram[registers[n]];
+        } else if (code == 9) {
+            ram[registers[n]] = registers[d];
+        }
     }
-    return num;
 }
 
 int main() {
-    int arr[3] = {2, 2, 3};
-    int num = arr2num(arr);
-    printf("%d\n", num);
+    int ram[1000] = {0};
+    int i = 0;
+    printf("Enter the instructions one by one. Enter -1 to stop:\n");
+    while(1){
+        scanf("%d", &ram[i]);
+        if(ram[i]==-1)
+            break;
+        i++;
+    }
+    interpreter(ram);
+    return 0;
 }
