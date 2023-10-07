dd29b407c40be0957ffc25a672694f915ab80c9f
jakoblund11 <98823878+jakoblund11@users.noreply.github.com>
2023-10-07 09:42:09 +0200

    Co-authored-by: Henrik Hoangkhanh Huynh <Khanrik@users.noreply.github.com>

--- challenge.c
+++ challenge.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int registers[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
 int arr2num (int arr[3]) {
     int num = 0;
     for (int i = 0; i < 3; i++) {
@@ -11,17 +9,9 @@ int arr2num (int arr[3]) {
     return num;
 }
 
-// IKKE DONE
-int num2arr (int num) {
-    int arr[3];
-    for (int i = 0; i < 3; i++) {
-        arr[i] = num % 10;
-        num /= 10;
-    }
-    return arr;
-}
-
-void code_x (int x, int y, int z, int *inp) {
+void code_x (int x, int y, int z, int inp[1000][3], int *RAM, int registers[10]) {
+    int temp1;
+    
     switch (x)
     {
     case 2:
@@ -37,6 +27,7 @@ void code_x (int x, int y, int z, int *inp) {
         registers[y] = registers[z];
         break;
     case 6:
+        printf("doing something\n");
         registers[y] = (registers[y] + registers[z]) % 1000;
         break;
     case 7:
@@ -46,18 +37,24 @@ void code_x (int x, int y, int z, int *inp) {
         registers[y] = arr2num(inp[z]);
         break;
     case 9:
-        inp[z] = registers[y];
+        temp1 = registers[y];
+        inp[z][0] = temp1 / 100;
+        inp[z][1] = (temp1 - inp[z][0] * 100) / 10;
+        inp[z][2] = (temp1 - inp[z][0] * 100 - inp[z][1] * 10);
         break;
     case 0:
-        
+        if (registers[z] != 0)
+            *RAM = registers[y];
         break;
     default:
         break;
     }
 }
 
+
 int main() {
     int inp[1000][3];
+    int registers[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     for (int i = 0; i < 1000; i++) {
         for (int j = 0; j < 3; j++) {
             inp[i][j] = 0;
@@ -66,31 +63,67 @@ int main() {
 
     int i = 0;
     int temp;
-    while (1) {
-        scanf("%d", &temp);
-        inp[i][0] = temp / 100;
-        if (temp / 100 == 1) {
-            break;
-        }
-        inp[i][1] = (temp - inp[i][0] * 100) / 10;
-        inp[i][2] = (temp - inp[i][0] * 100 - inp[i][1] * 10);
-        printf("%d %d %d\n", inp[i][0], inp[i][1], inp[i][2]);
-        i++;
+    // while (1) {
+    //     scanf("%d", &temp);
+    //     inp[i][0] = temp / 100;
+    //     if (temp / 100 == 1) {
+    //         break;
+    //     }
+    //     inp[i][1] = (temp - inp[i][0] * 100) / 10;
+    //     inp[i][2] = (temp - inp[i][0] * 100 - inp[i][1] * 10);
+    //     i++;
+    // }
+
+    int case_inp[12][3] = { {2, 9, 9},
+                            {4, 9, 2},
+                            {4, 9, 5},
+                            {3, 9, 9},
+                            {4, 9, 2},
+                            {4, 9, 5},
+                            {3, 9, 9},
+                            {2, 8, 3},
+                            {2, 7, 9},
+                            {6, 8, 9},
+                            {0, 7, 8},
+                            {1, 0, 0} };
+    
+    for (int i = 0; i < 12; i++) {
+        inp[i][0] = case_inp[i][0];
+        inp[i][1] = case_inp[i][1];
+        inp[i][2] = case_inp[i][2];
     }
+
+    printf("Input registered\n");
     
     int RAM = 0;
     int counter = 0;
+    int NineCount = 0;
     while (RAM < 1000) {
         counter++;
         if (inp[RAM][0] == 1) {
+            printf("stop");
             break;
         }
-        code_x(inp[RAM][0], inp[RAM][1], inp[RAM][2], &inp);
+        if (inp[RAM][0] == 0 && inp[RAM][1] == 0 && inp[RAM][2] == 0) {
+            break;
+        }
+        code_x(inp[RAM][0], inp[RAM][1], inp[RAM][2], inp, &RAM, registers);
+        printf("Current RAM = %d\n", RAM);
         RAM++;
+        printf("Current RAM = %d\n", RAM);
+        // if (RAM == 9) {
+        //     printf("%d %d %d\n", inp[RAM][0], inp[RAM][1], inp[RAM][2]);
+        //     NineCount++;
+        //     if (NineCount > 3) {
+        //         break;
+        //     }
+        // }
+        // else {
+        //     NineCount = 0;
+        // }
+        // RAM++;
     }
     
-    printf("%c\n", "001"[0]);
-    printf("%d\n", registers[0]);
     printf("%d\n", counter);
     return 0;
 }
